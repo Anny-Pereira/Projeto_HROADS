a548f9b7d397c192be6137c84e1087553e5b34d0
Gustavo Rezende <gustrpaz@gmail.com>
2021-09-16 21:54:07 -0300

    Crud de usuario tipoUsuario classes classeHabilidade

--- Backend/Senai_HROADS_WebApi/Senai_HROADS_WebApi/Controllers/LoginController.cs
+++ Backend/Senai_HROADS_WebApi/Senai_HROADS_WebApi/Controllers/LoginController.cs
@@ -25,37 +25,7 @@ namespace Senai_HROADS_WebApi.Controllers
         {
             Usuario usuarioBuscado = _UsuarioRepository.Login(login.Email, login.Senha);
 
-            if (usuarioBuscado != null)
-            {
-                //return Ok(usuarioBuscado);
-
-                var minhasclaims = new[]
-                {
-                    new Claim(JwtRegisteredClaimNames.Email, usuarioBuscado.Email),
-                    new Claim(JwtRegisteredClaimNames.Jti, usuarioBuscado.IdUsuario.ToString()),
-                    new Claim(ClaimTypes.Role, usuarioBuscado.IdTipoUsuario.ToString()),
-                    new Claim("Claim Personalizada", "Anny e Rezende")
-                };
-
-                var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("Autenticacao_ChaveInLock"));
-
-                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-
-                var MeuToken = new JwtSecurityToken(
-                    issuer: "Inlock.webAPI",
-                    audience: "Inlock.webAPI",
-                    claims: minhasclaims,
-                    expires: DateTime.Now.AddMinutes(30),
-                    signingCredentials: creds
-                    );
-
-                return Ok(new
-                {
-                    token = new JwtSecurityTokenHandler().WriteToken(MeuToken)
-                });
-            }
-
-            return NotFound("Email ou senha inválidos");
+          
         }
     }
 }
